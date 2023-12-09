f54fa0e6311ad7e090856f3e3d229fb1b44c7573
artm22es <artm22es@gmail.com>
2023-12-09 10:34:26 +0100

    upd-task-2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,13 +1,33 @@
 import { StatisticItem } from 'components';
+
 import { StatisticsList, StatisticTitle } from './Statistics.styled';
 import { FaRegThumbsUp } from 'react-icons/fa';
 import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
 import { GiTreeDoor } from 'react-icons/gi';
 
-export const Statistics = () => {
+const icons = [
+  <FaRegThumbsUp />,
+  <MdPeople />,
+  <MdOutlineProductionQuantityLimits />,
+  <GiTreeDoor />,
+];
+
+export const Statistics = ({ title, stats }) => {
   return (
     <>
-      <div>StatisticsList</div>
+      <StatisticTitle>{title}</StatisticTitle>
+      <StatisticsList>
+        {stats.map(({ id, title, total }, idx) => {
+          return (
+            <StatisticItem
+              key={id}
+              title={title}
+              total={total}
+              icon={icons[idx]}
+            />
+          );
+        })}
+      </StatisticsList>
     </>
   );
 };
