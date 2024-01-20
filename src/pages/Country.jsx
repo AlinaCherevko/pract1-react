ee8f1c44a31609807d7aa27dc265103a9773b198
Alina Cherevko <alinapavluhina@ukr.net>
2024-01-20 13:03:56 +0200

    Done

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,10 +1,24 @@
 import { Section, Container, CountryInfo, Loader } from 'components';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchCountry } from 'service/country-service';
 
 export const Country = () => {
+  const [result, setResult] = useState([]);
+  const { countryId } = useParams();
+
+  useEffect(() => {
+    const detDetails = async () => {
+      const result = await fetchCountry(countryId);
+      setResult(result);
+    };
+
+    detDetails();
+  }, [countryId]);
   return (
     <Section>
       <Container>
-        <h2>Country</h2>
+        <CountryInfo {...result}></CountryInfo>
       </Container>
     </Section>
   );
